b29533c9aa5e568da9467ce53b41317dd19bece4
ksrntheja <srntkolla@gmail.com>
2019-11-20 14:25:23 +0530

    04-Added Route Configurations.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,48 @@ import { AppComponent } from './app.component';
 
 import { RouterModule, Routes } from '@angular/router';
 
+const appRoutes: Routes = [
+  {
+    path: 'crisis-center',
+    component: CrisisListComponent
+  },
+  {
+    path: 'hero/:id',
+    component: HeroDetailComponent
+  },
+  {
+    path: 'heroes',
+    component: HeroListComponent,
+    data:
+          {
+            title:
+            'Heroes List'
+          }
+  },
+  {
+    path: '',
+    redirectTo: '/heroes',
+    pathMatch: 'full'
+  },
+  {
+    path: '**',
+    component: PageNotFoundComponent
+  }
+];
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(
+      appRoutes,
+      {
+        enableTracing: true // <-- debugging purposes only
+      }
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
